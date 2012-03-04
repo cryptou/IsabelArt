b4dd88890a39ec8cf43ef5679d2953e97822f756
cryptou <cryptou@gmail.com>
2012-03-04 18:27:02 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IsabelArt::Application.config.secret_token = '1a5218d4c784f214893b9b11c64e20bc2af268925c854d17f1ea38a52eb24514312c875b7dbcf8946c70c3853bf78bcfb3c34b5c7aa364742d05606f8b1e237a'
