6ce3f11c98e87ee7fa63b9da803397f2a68926d3
cryptou <cryptou@gmail.com>
2012-03-13 19:34:45 +0800

    created a category controller

---
+++ app/controllers/category_controller.rb
@@ -0,0 +1,16 @@
+class CategoryController < ApplicationController
+  def china
+  end
+
+  def burma
+  end
+
+  def asia
+  end
+
+  def gandhara
+  end
+
+  def porcelain
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 IsabelArt::Application.routes.draw do
-  
-  get "category/china"
-
-  get "category/burma"
-
-  get "category/asia"
-
-  get "category/gandhara"
-
-  get "category/porcelain"
 
   scope ":locale", :locale => /en|fr|es/ do
-    match '/contact', to: 'static_pages#contact'
-    match '/home', to: 'static_pages#home'
+    match '/contact',    to: 'static_pages#contact'
+    match '/home',      to: 'static_pages#home'
+    match '/china',       to: 'category#china'
+    match '/burma',     to: 'category#burma'
+    match '/asia',         to: 'category#asia'
+    match '/gandhara', to: 'category#gandhara'
+    match '/porcelain', to: 'category#porcelain'
   end
   
   match '/:locale', to: 'static_pages#home'
